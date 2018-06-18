f9ee65f28f22fe5d7be836542165064722436ec5
Dmitry Knyazev <4dmitry.kn@gmail.com>
2018-06-18 12:38:49 -0400

    add routes: register, login + passport

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users.js');
 const posts = require('./routes/api/posts.js');
@@ -13,20 +15,18 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-
-
 //==============//
 //App config
 //==============//
-
-//User routes
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+  //User routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
-
-app.get('/', (req, res) => {
-  res.send("Hello");
-})
+  //Passport middleware
+app.use(passport.initialize());
+require('./config/passport.js')(passport);
 
 const port = process.env.PORT || 3000;
 
--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://Admin:admink0@ds163650.mlab.com:63650/socialdev'
+  mongoURI: 'mongodb://Admin:admink0@ds163650.mlab.com:63650/socialdev',
+  secretOrKey: 'secret'
 }
