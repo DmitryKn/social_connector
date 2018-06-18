486553339edc7c77151227b837199c020c6366b8
Dmitry Knyazev <4dmitry.kn@gmail.com>
2018-06-18 11:12:59 -0400

    express routes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users.js');
+const posts = require('./routes/api/posts.js');
+const profile = require('./routes/api/profile.js');
 
 //DB Config
 const db = require("./config/keys.js").mongoURI;
@@ -9,6 +12,17 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+
+
+//==============//
+//App config
+//==============//
+
+//User routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 app.get('/', (req, res) => {
   res.send("Hello");
