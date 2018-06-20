8d0ab45140e1542aaf673934e17ac5bbc053c865
Dmitry Knyazev <4dmitry.kn@gmail.com>
2018-06-20 19:01:41 -0400

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Layout/Navbar.js';
+import Footer from './components/Layout/Footer.js';
+import Landing from './components/Layout/Landing.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
