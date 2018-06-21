4882fd41616ea1d8065a5e36ed0d50b6047bd607
Dmitry Knyazev <4dmitry.kn@gmail.com>
2018-06-20 22:28:34 -0400

    setup react-router

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Navbar from './components/Layout/Navbar.js';
 import Footer from './components/Layout/Footer.js';
 import Landing from './components/Layout/Landing.js';
+import Login from './components/Auth/Login.js';
+import Register from './components/Auth/Register.js';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Landing />
+        <Route exact path ="/" component={ Landing } />
+        <div className="container">
+          <Route exact path="/register" component={ Register } />
+          <Route exact path="/login" component={ Login } />
+        </div>
         <Footer />
       </div>
     );
