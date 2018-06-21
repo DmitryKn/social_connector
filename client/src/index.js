e1717d60141c8be7edfd2eb2f5e15c7714b66a79
Dmitry Knyazev <4dmitry.kn@gmail.com>
2018-06-21 13:13:09 -0400

    Redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import combineReducers from './reducers/combine.js';
+
+const initialState = {};
+const middleware = [thunk]
+const store = createStore(
+  combineReducers,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
    document.getElementById('root'));
 registerServiceWorker();
