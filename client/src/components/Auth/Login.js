845088e40ff8ad133cc2383442c5b6dd164723de
Dmitry Knyazev <4dmitry.kn@gmail.com>
2018-06-21 10:48:41 -0400

    add Login form

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+state = {
+  email: "",
+  password: "",
+  errors: {}
+}
+onChange(event) {
+  this.setState({[event.target.name]: event.target.value})
+}
+onSubmit(event) {
+  event.preventDefault();
+  const User = {
+    email: this.state.email,
+    password: this.state.password
+  }
+  console.log(User);
+}
+
 
   render() {
     return (
@@ -10,12 +27,16 @@ class Login extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
               <p className="lead text-center">Sign in to your DevConnector account</p>
-              <form action="dashboard.html">
-                <div clasNames="form-group">
-                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
+              <form onSubmit={this.onSubmit.bind(this)}>
+                <div className="form-group">
+                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email"
+                    value={this.state.email}
+                    onChange={this.onChange.bind(this)}/>
                 </div>
-                <div clasNames="form-group">
-                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                <div className="form-group">
+                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password"
+                    value={this.state.password}
+                    onChange={this.onChange.bind(this)}/>
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
